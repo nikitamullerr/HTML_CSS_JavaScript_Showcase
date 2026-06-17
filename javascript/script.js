f0c3dba9a1f67444d22fe3d4581d4ca67991898d
nikitamuller <nikitamuller1005@gmail.com>
2026-06-17 08:44:58 +0200

    added the css, and javascript to the home page

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* Rushin */
 
 
@@ -73,3 +74,70 @@ if (privacyLink) {
 
 
 /* Lukho */
+=======
+console.log("JavaScript loaded");
+const bookButtons = document.querySelectorAll(".book-btn");
+
+bookButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const tutorCard = button.closest(".tutor-card");
+    const tutorName = tutorCard.querySelector(".tutor-name").textContent;
+
+    alert(`You selected ${tutorName}`);
+  });
+});
+
+// filtering buttons - find a tutor page
+document.addEventListener("DOMContentLoaded", () => {
+
+  const searchBtn = document.querySelector(".search-btn");
+  const tutors = document.querySelectorAll(".tutor-card");
+  const noResults = document.getElementById("no-results");
+
+  searchBtn.addEventListener("click", () => {
+
+    const selectedCourse = document.getElementById("course").value;
+    const selectedPrice = document.getElementById("price").value;
+    const selectedAvailability = document.getElementById("availability").value;
+    const selectedStyle = document.getElementById("learning-style").value;
+
+    let hasMatch = false;
+
+    tutors.forEach((tutor) => {
+
+      const course = tutor.dataset.course;
+      const price = tutor.dataset.price;
+      const availability = tutor.dataset.availability;
+      const style = tutor.dataset.style;
+
+      const courseMatch =
+        selectedCourse === "Select a subject" || selectedCourse === course;
+
+      const priceMatch =
+        selectedPrice === "Any Price" || selectedPrice === price;
+
+      const availabilityMatch =
+        selectedAvailability === "Any Availability" || selectedAvailability === availability;
+
+      const styleMatch =
+        selectedStyle === "Any Style" || selectedStyle === style;
+
+      if (courseMatch && priceMatch && availabilityMatch && styleMatch) {
+        tutor.style.display = "block";
+        hasMatch = true;
+      } else {
+        tutor.style.display = "none";
+      }
+    });
+
+    // show/hide no results message
+    if (hasMatch) {
+      noResults.style.display = "none";
+    } else {
+      noResults.style.display = "block";
+    }
+
+  });
+
+});
+>>>>>>> efff260 (added the css, and javascript to the home page)

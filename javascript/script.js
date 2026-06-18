221c52757860450c91d65ddfac87f991e398ff5e
nikitamuller <nikitamuller1005@gmail.com>
2026-06-18 12:23:12 +0200

    fixed nav bar and footer in responsive design

--- javascript/script.js
+++ javascript/script.js
@@ -13,7 +13,7 @@ bookButtons.forEach(button => {
     const tutorName = tutorCard.querySelector(".tutor-name").textContent;
 
     selectedTutor.textContent =
-      `You selected ${tutorName}.`;
+      `You selected ${tutorName}.`
 
     modal.style.display = "flex";
   });
